69e5c88770871c71109e4001aadf2e3913072821
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2020-01-10 23:15:43 +0530

    update cnn

--- cat_dog_classification_with_cnn/cnn.py
+++ cat_dog_classification_with_cnn/cnn.py
@@ -1,40 +1,54 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan  9 22:08:29 2020
+# Convolutional Neural Network
 
-@author: dhruv
-"""
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
 
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Conv2D
-from keras.layers import Dense
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
+from keras.layers import Dense
+import time
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
 
 from keras.preprocessing.image import ImageDataGenerator
 
-Classifier = Sequential()
-
-## conv2d with 32 filters , 3x3 filter size strides = 1x1 
-Classifier.add(Conv2D(32,(3,3),input_shape=(64,64,3)),activation='relu')
-
-
-Classifier.add(MaxPooling2D(pool_size=(2,2),))
-
-Classifier.add(Flatten())
-
-Classifier.add(Dense(units=128,activation='relu'))
-Classifier.add(Dense(units=1,activation='sigmoid'))
-
-
-Classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
-
-
-
-
 train_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,
-                                   zoom_range = 0.2 ,
+                                   zoom_range = 0.2,
                                    horizontal_flip = True)
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
@@ -49,8 +63,32 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             batch_size = 32,
                                             class_mode = 'binary')
 
-Classifier.fit_generator(training_set,
+## start time of training
+from datetime import datetime
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+
+classifier.fit_generator(training_set,
                          steps_per_epoch = 8000,
                          epochs = 25,
                          validation_data = test_set,
-                         validation_steps = 2000)    
+                         validation_steps = 2000)
+
+
+classifier.save('h5')
+
+## training end
+print("ends")
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+
+
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load('dataset/single_prediction/cat_or_dog_1.jpg',target_size = (64,64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image,axis=0)
+resu
